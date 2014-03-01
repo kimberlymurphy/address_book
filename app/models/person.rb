f8e4d9518938d283cb96bcaf63f4143118ac70d8
kimberlymurphy <murphykimberlyann@gmail.com>
2014-03-01 14:01:38 -0700

    End of class

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,9 @@
 class Person < ActiveRecord::Base
+  validates :first_name, :presence => true #, on: :update
+  validates :last_name, presence: {message: "You don't have a last name?"}
 end
+
+
+#Has key syntax
+#  :key => value
+#  key: value
